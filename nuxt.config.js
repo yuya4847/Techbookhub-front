c62bb6612472a984e721bf218823e95db619a76e
yuya <yuya004847yy@gmail.com>
2022-05-29 14:40:30 +0900

    Add Vuetify Module

--- nuxt.config.js
+++ nuxt.config.js
@@ -34,7 +34,8 @@ export default {
   // Modules for dev and build (recommended): https://go.nuxtjs.dev/config-modules
   buildModules: [
     // https://go.nuxtjs.dev/eslint
-    '@nuxtjs/eslint-module'
+    '@nuxtjs/eslint-module',
+    '@nuxtjs/vuetify'
   ],
 
   // Modules: https://go.nuxtjs.dev/config-modules
@@ -49,7 +50,29 @@ export default {
     // baseURL: '/'
   },
 
+  vuetify: {
+    theme: {
+      themes: {
+        light: {
+          primary: '4080BE',
+          info: '4FC1E9',
+          success: '44D69E',
+          warning: 'FEB65E',
+          error: 'FB8678',
+          background: 'f6f6f4'
+        }
+      }
+    }
+  },
+
   // Build Configuration: https://go.nuxtjs.dev/config-build
   build: {
-  }
+    hardSource: true
+  },
+
+  generate: {
+    fallback: true,
+  },
+
+  watch: ['static'],
 }
