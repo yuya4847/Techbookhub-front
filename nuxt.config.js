8700d83e0074434064077794713f3d8d45e651f7
yuya <yuya004847yy@gmail.com>
2022-06-06 23:57:33 +0900

    Add user form components

--- nuxt.config.js
+++ nuxt.config.js
@@ -77,6 +77,7 @@ export default {
   i18n: {
     locales: ['ja', 'en'],
     defaultLocale: 'ja',
+    strategy: 'no_prefix',
     // Doc: https://kazupon.github.io/vue-i18n/api/#properties
     vueI18n: {
       fallbackLocale: 'ja',
--- pages/signup.vue
+++ pages/signup.vue
@@ -1,11 +1,34 @@
 <template>
-  <div>
-    signup.vue
-  </div>
+  <user-form-card>
+    <template
+      #user-form-card-content
+    >
+      <v-form
+        v-model="isValid"
+      >
+        <user-form-name />
+        <user-form-email />
+        <user-form-password />
+        <v-btn
+          :disabled="true"
+          block
+          color="appblue"
+          class="white--text"
+        >
+          登録する
+        </v-btn>
+      </v-form>
+    </template>
+  </user-form-card>
 </template>
 
 <script>
 export default {
-  layout: 'before-login'
+  layout: 'before-login',
+  data () {
+    return {
+      isValid: false
+    }
+  }
 }
 </script>
