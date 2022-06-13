88e98040ac7f023c4d52786b16e3d438217f119b
yuya <yuya004847yy@gmail.com>
2022-06-13 18:19:40 +0900

    Add projects.vue layouts

--- plugins/my-inject.js
+++ plugins/my-inject.js
@@ -3,10 +3,23 @@ class MyInject {
     this.app = ctx.app
   }
 
+  // i18nページタイトル変換
   pageTitle (routeName) {
     const jsonPath = `pages.${routeName.replace(/-/g, '.')}`
     const title = this.app.i18n.t(jsonPath)
     return title
+  }
+
+  // 日付のフォーマットの変換
+  dateFormat (dateStr) {
+    const dateTimeFormat = new Intl.DateTimeFormat(
+      'ja', { dateStyle: 'medium', timeStyle: 'short' }
+    )
+    return dateTimeFormat.format(new Date(dateStr))
+  }
+
+  projectLinkTo (id, name = 'project-id-dashboard') {
+    return { name, params: { id } }
   }
 }
 
